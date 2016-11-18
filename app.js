6c71510c560ef28a6a55b4ebafe49e621164f817
kitsana waiyakun <jozeaza18@hotmail.com>
2016-11-18 12:58:55 +0700

    add service

--- app.js
+++ app.js
@@ -1,41 +1,14 @@
 angular.module('todosApp',[])
 .service('contactsService', function(){
-    var t = this
-    t.contacts = [
+    var self = this
+    self.contacts = [
       { title: 'Waravut Worabut', done: false},
-      { title: 'Weera Kasetsin', done: true}
+      { title: 'kitsana waiyakun', done: true}
     ]
-    t.list = function(){
-      return t.contacts
+    self.list = function(){
+      return self.contacts
     }
+    self.add = function (contact) {
+  		self.contacts.push(contact)
+  	}
   })
-
-  .controller('ListContactController', function ($scope, contactService) {
-   $scope.contacts = contactsService.list()
-   $scope.newContacts = {
-     Checkbox: '',
-     title: '',
-     done: ''
-   };
-
-   $scope.save = function (){
-    var contact = new Contact();
-    contact.Checkbox = $scope.newContact.Checkbox;
-    contact.Title = $scope.newContact.Title;
-    contact.Done = $scope.newContact.Done;
-
-    $scope.contacts.push(contact);
-
-    $scope.newContact.Checkbox= '';
-    $scope.newContact.Title = '';
-    $scope.newContact.Done = '';
-  };
-
-
-  function Contact(){
-      this.Checkbox = '';
-      this.Title= '';
-      this.Done = '';
-    };
-
-});
